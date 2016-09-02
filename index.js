c9c9f9411bc4fe386774545b72df37b84a7f4b8a
astur <astur@yandex.ru>
2016-09-02 13:22:15 +0300

    Initial commit (v0.0.1)

---
+++ index.js
@@ -0,0 +1,37 @@
+var MongoClient = require('mongodb').MongoClient;
+var mongoString = process.env.MONGOLAB_URI ||
+    'mongodb://localhost:27017/test';
+
+module.exports = function (keys, collectionName, limit, callback){
+    MongoClient.connect(mongoString, function(err, db) {
+        if(err){
+            callback(err);
+        }
+        var flats = db.collection(collectionName);
+        var bulk = flats.initializeUnorderedBulkOp();
+        var counter = 0;
+
+        callback(null, function(doc){
+            if (!doc) {
+                try{
+                    bulk.execute(function(){
+                        db.close();
+                    });
+                } catch(e) {
+                    db.close();
+                }
+            } else {
+                var q = {};
+                keys.forEach(function(v){
+                    q[v] = doc[v];
+                });
+                bulk.find(q).upsert().replaceOne(doc);
+                counter ++;
+                if(counter % limit === 0){
+                    bulk.execute();
+                    bulk = flats.initializeUnorderedBulkOp();
+                }
+            }
+        });
+    });
+};
