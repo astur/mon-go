c393d81ca619e61bd8b5ac87bd2ac509124e3e92
astur <astur@yandex.ru>
2016-09-19 17:34:45 +0300

    multiwrapping (v0.0.4)

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoString = process.env.MONGOLAB_URI ||
     'mongodb://localhost:27017/test';
 
-var finalResult = {
-    nModified: 0,
-    nUpserted: 0,
-};
-
-function updateFinalResult(result){
-    finalResult.nModified += result.nModified;
-    finalResult.nUpserted += result.nUpserted;
-}
-
 module.exports = function (keys, collectionName, limit, callback){
     MongoClient.connect(mongoString, function(err, db) {
         if(err){
@@ -21,31 +11,48 @@ module.exports = function (keys, collectionName, limit, callback){
         keys.forEach(function(v){
             k[v] = 1;
         });
-        var flats = db.collection(collectionName);
-        flats.createIndex(k, {unique: true});
-        var bulk = flats.initializeUnorderedBulkOp();
-        var counter = 0;
 
-        callback(null, function(doc){
-            if (typeof doc === 'function') {
-                bulk.execute()
-                    .then(updateFinalResult)
-                    .then(function(){
-                        doc(finalResult);
-                    });
-            } else {
-                var q = {};
-                keys.forEach(function(v){
-                    q[v] = doc[v];
-                });
-                bulk.find(q).upsert().replaceOne(doc);
-                counter ++;
-                if(counter % limit === 0){
-                    bulk.execute()
-                        .then(updateFinalResult);
-                    bulk = flats.initializeUnorderedBulkOp();
-                }
+        var save = (function(cName){
+            var finalResult = {
+                nModified: 0,
+                nUpserted: 0,
+            };
+
+            function updateFinalResult(result){
+                finalResult.nModified += result.nModified;
+                finalResult.nUpserted += result.nUpserted;
             }
-        }, db);
+
+            var collection = db.collection(cName);
+            collection.createIndex(k, {unique: true});
+            var bulk = collection.initializeUnorderedBulkOp();
+            var counter = 0;
+
+            return function(doc){
+                if (typeof doc === 'function') {
+                    bulk.execute()
+                        .then(updateFinalResult)
+                        .then(function(){
+                            doc(finalResult);
+                        });
+                } else {
+                    var q = {};
+                    keys.forEach(function(v){
+                        q[v] = doc[v];
+                    });
+                    bulk.find(q).upsert().replaceOne(doc);
+                    counter ++;
+                    if(counter % limit === 0){
+                        bulk.execute()
+                            .then(updateFinalResult);
+                        bulk = collection.initializeUnorderedBulkOp();
+                    }
+                }
+            };
+        })(collectionName);
+
+        callback(null, save, db);
+
+
     });
 };
